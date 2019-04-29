fc3ed7cca2ba1d2f40d43deb9603b3cc79fbb248
kim jinkyum <44569994+kimjinkyum@users.noreply.github.com>
2019-04-30 02:08:15 +0900

    Add files via upload

---
+++ LoginActivity.java
@@ -0,0 +1,16 @@
+package com.app.termproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
