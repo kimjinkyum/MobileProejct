4dce5f129cc11cf7091cb35c817e2d9a96b1ec2b
Jung-Juhwan <44596066+Jung-Juhwan@users.noreply.github.com>
2019-06-03 06:32:07 +0900

    인트로

--- TermProject/app/src/main/java/com/app/termproject/MainActivity.java
+++ TermProject/app/src/main/java/com/app/termproject/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         timer.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                Intent intent=new Intent(MainActivity.this,WelcomeActivity.class);
                 startActivity(intent);
                 finish();
             }
